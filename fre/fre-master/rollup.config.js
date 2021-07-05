aea8253139342462ec106c08b4dc0071fc58e3dc
萧萧落雨 <8071489+xluoyu@user.noreply.gitee.com>
2021-07-05 17:35:57 +0800

    update

---
+++ fre/fre-master/rollup.config.js
@@ -0,0 +1,23 @@
+import { terser } from 'rollup-plugin-terser'
+import typescript from 'rollup-plugin-typescript2'
+
+const plugins = [
+  typescript({
+    tsconfig: 'tsconfig.json',
+    removeComments: true,
+    useTsconfigDeclarationDir: true,
+  }),
+  terser({
+    include: ['fre.js'],
+  }),
+]
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    { file: 'dist/fre.umd.js', format: 'umd', name: 'fre', sourcemap: true },
+    { file: 'dist/fre.js', format: 'esm', sourcemap: true },
+    { file: 'dist/fre.esm.js', format: 'esm', sourcemap: true },
+  ],
+  plugins,
+}
